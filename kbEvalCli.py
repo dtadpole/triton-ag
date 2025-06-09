d6a96e15029d93dc56e9eaea34c257517192e156
D.Tadpole <gsk390@gmail.com>
2025-06-09 03:49:17 +0000

    minor fix

--- kbEvalCli.py
+++ kbEvalCli.py
@@ -107,6 +107,7 @@ def compile_and_eval_kernel(
             )
             return result
         finally:
+            torch.cuda.synchronize(device=device)
             lock.release()
             # check lockfile modified time
             if os.path.exists(lock_file):
@@ -343,7 +344,7 @@ if __name__ == "__main__":
     parser.add_argument("--generated_code", type=str, default="elemAddCuda.py")
     parser.add_argument("--measure_performance_ref", action="store_true")
     parser.add_argument("--device-list", type=str, default="4")
-    parser.add_argument("--max-jobs", type=int, default=8)
+    parser.add_argument("--max-jobs", type=int, default=12)
     parser.add_argument("--verbose", action="store_true")
     args = parser.parse_args()
 
