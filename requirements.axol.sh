941718d5207c24f418dc545cf0135663bca9e3aa
D.Tadpole <gsk390@gmail.com>
2025-06-20 23:50:02 +0000

    tweak

--- requirements.axol.sh
+++ requirements.axol.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 uv pip install packaging setuptools wheel
-uv pip install torch
+uv pip install torch --index-url https://download.pytorch.org/whl/cu128
 uv pip install awscli pydantic psutil
 
 uv pip install --no-build-isolation axolotl[deepspeed]
-uv pip install flash-attn
+uv pip install -U flash-attn --no-build-isolation
