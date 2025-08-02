46843fcb424305b9d7322210e8000e2eae4129c0
D.Tadpole <gsk390@gmail.com>
2025-08-02 03:56:50 +0000

    reliability

--- kbEvalServer.py
+++ kbEvalServer.py
@@ -171,7 +171,7 @@ async def kb_eval_ref(
 
         eval_tag = "reference"
         # pre-compile the reference code
-        command = f"timeout --foreground -signal=SIGTERM --kill-after=5s 270s python kbEvalCli.py --wd {temp_dir} --run_tag {run_tag} --model_tag {model_tag} --task_tag {task_tag} --eval_tag {eval_tag} --reference_code {reference_file_path} --measure_reference --device-list {','.join([str(device) for device in DEVICES])} --code_type pytorch --quiet"
+        command = f"timeout --foreground --signal=SIGTERM --kill-after=5s 270s python kbEvalCli.py --wd {temp_dir} --run_tag {run_tag} --model_tag {model_tag} --task_tag {task_tag} --eval_tag {eval_tag} --reference_code {reference_file_path} --measure_reference --device-list {','.join([str(device) for device in DEVICES])} --code_type pytorch --quiet"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
@@ -228,7 +228,7 @@ async def kb_eval_ref(
             compiled=False,
             correctness=False,
             metadata={
-                "processing_error": f"Unexpected error: {type(e).__name__}: {str(e)}",
+                "processing_error": f"[kb_eval_ref] Cannot generate the result file in time. Unexpected error: {type(e).__name__}: {str(e)}",
                 "retriable": True, # if the error is retriable, the client will retry the request
             },
             runtime=-1.0,
@@ -281,7 +281,7 @@ async def kb_eval(
         # logger.info(f"[KB Eval] [{eval_tag}] generated_file_path: [{generated_file_path}]")
 
         # pre-compile the generated code
-        command = f"timeout --foreground -signal=SIGTERM --kill-after=5s 270s python kbEvalCli.py --wd {temp_dir} --run_tag {run_tag} --model_tag {model_tag} --task_tag {task_tag} --eval_tag {eval_tag} --reference_code {reference_file_path} --generated_code {generated_file_path} --device-list {','.join([str(device) for device in DEVICES])} --code_type {code_type} --quiet"
+        command = f"timeout --foreground --signal=SIGTERM --kill-after=5s 270s python kbEvalCli.py --wd {temp_dir} --run_tag {run_tag} --model_tag {model_tag} --task_tag {task_tag} --eval_tag {eval_tag} --reference_code {reference_file_path} --generated_code {generated_file_path} --device-list {','.join([str(device) for device in DEVICES])} --code_type {code_type} --quiet"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
@@ -335,7 +335,7 @@ async def kb_eval(
             compiled=False,
             correctness=False,
             metadata={
-                "processing_error": f"Unexpected error: {type(e).__name__}: {str(e)}",
+                "processing_error": f"[kb_eval] Cannot generate the result file in time. Unexpected error: {type(e).__name__}: {str(e)}",
                 "retriable": True, # if the error is retriable, the client will retry the request
             },
             runtime=-1.0,
