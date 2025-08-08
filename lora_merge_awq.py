fe64f33d85a7506e2a580ad174af0e5944865df8
Jingbo Liu <jingbo25@fb.com>
2025-08-08 14:28:50 -0700

    small bug fix

--- lora_merge_awq.py
+++ lora_merge_awq.py
@@ -225,7 +225,7 @@ def main(config_path):
 
         # Load the full precision model from device for AWQ
         model = AutoAWQForCausalLM.from_pretrained(tem_full_precision_model,
-                                            device_map="cuda",
+                                            device_map="auto",
                                             torch_dtype=torch.float16,
                                             safetensors=True)
 
