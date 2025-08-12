3c992e2c1fe4c1f383f7feabc6389c035fabd176
dtadpole <gsk390@gmail.com>
2025-08-12 18:17:12 +0000

    tweak

--- inferenceComposer.py
+++ inferenceComposer.py
@@ -343,7 +343,7 @@ async def main():
     parser.add_argument("--num_samples", type=int, default=2)
     parser.add_argument("--num_generations", type=int, default=2)
     parser.add_argument("--num_turns_per_generation", type=int, default=4)
-    parser.add_argument("--use_global_queue", type=str, default="inference.codeGenEval") # this is the task_name of the global queue
+    parser.add_argument("--use_global_queue", type=str, default=None) # this is the task_name of the global queue
     parser.add_argument("--proc_id", type=str, default=None)
     parser.add_argument("--module_file", type=str, default="inference/codeGenEval.module.yaml")
     parser.add_argument("--prompt_file", type=str, default="inference/triton.prompt.yaml")
