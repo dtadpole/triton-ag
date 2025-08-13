9f525261a93c041d71a45ab833ab957f829ccda8
Zhen Chen <zhenc@fb.com>
2025-08-13 01:16:02 -0700

    for devserver

---
+++ podman/run-vllm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export HF_HUB_DISABLE_XET=1
+export HF_HUB_ENABLE_HF_TRANSFER=0
+
+podman run -it \
+    --security-opt=label=disable \
+    --device nvidia.com/gpu=all \
+    --network host --ipc=host \
+    -v ~/.cache/huggingface:/root/.cache/huggingface \
+    -v ~/.trainer:/root/.trainer \
+    --env "HF_TOKEN=`cat ~/.keys/huggingface.api.key`" \
+    -e HTTP_PROXY -e HTTPS_PROXY -e NO_PROXY \
+    -e http_proxy -e https_proxy -e no_proxy \
+    docker://dtadpole/vllm:v0.7
