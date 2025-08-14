715caa53bfeddf857ba8a34d8de4e801b5d6511b
Amir Khojaste <akhojast@meta.com>
2025-08-13 22:55:27 -0700

    adding reasoning example of gpt-oss model

--- vllm_test/test_gpt_oss_120b.py
+++ vllm_test/test_gpt_oss_120b.py
@@ -19,22 +19,39 @@ def get_weather(city: str):
 
 
 async def main():
-    agent = Agent(
-        name="Assistant",
-        instructions="You only respond in haikus.",
-        model=OpenAIResponsesModel(
-            # model="openai/gpt-oss-120b",
-            model="hub/models--openai--gpt-oss-120b/snapshots/bc75b44b8a2a116a0e4c6659bcd1b7969885f423",
-            openai_client=AsyncOpenAI(
-                base_url="http://localhost:8000/v1",
-                api_key="EMPTY",
-            ),
-        ),
-        tools=[get_weather],
+
+    # agent = Agent(
+    #     name="Assistant",
+    #     instructions="You only respond in haikus.",
+    #     model=OpenAIResponsesModel(
+    #         # model="openai/gpt-oss-120b",
+    #         model="hub/models--openai--gpt-oss-120b/snapshots/bc75b44b8a2a116a0e4c6659bcd1b7969885f423",
+    #         openai_client=AsyncOpenAI(
+    #             base_url="http://localhost:8000/v1",
+    #             api_key="EMPTY",
+    #         ),
+    #     ),
+    #     tools=[get_weather],
+    # )
+    # result = await Runner.run(agent, "What's the weather in Tokyo?")
+    # print(result.final_output)
+
+    prompt = """
+    Write a bash script that takes a matrix represented as a string with 
+    format '[1,2],[3,4],[5,6]' and prints the transpose in the same format.
+    """
+
+    from openai import OpenAI
+
+    client = OpenAI(base_url="http://localhost:8000/v1", api_key="EMPTY")
+
+    response = client.responses.create(
+        model="hub/models--openai--gpt-oss-120b/snapshots/bc75b44b8a2a116a0e4c6659bcd1b7969885f423",
+        reasoning={"effort": "medium"},
+        input=[{"role": "user", "content": prompt}],
     )
 
-    result = await Runner.run(agent, "What's the weather in Tokyo?")
-    print(result.final_output)
+    print(response.output_text)
 
 
 if __name__ == "__main__":
