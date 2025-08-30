aa30ac4847888b8583629d25f4862ea61af25312
Zhen Chen <zhenc@fb.com>
2025-08-30 15:16:49 -0700

    enable wandb

--- kbEvalServer.py
+++ kbEvalServer.py
@@ -67,7 +67,6 @@ def verify_token(authorization: str = Header(None)):
 wandb_loggers = {} # {prefix_tag: wandb.Run}
 def _setup_wandb_logging(prefix_tag: str="auto", model_tag: str="local_qwen3-32b"):
     """Setup logging and tracking"""
-    return None
     if prefix_tag.startswith("auto"):
         return None
 
