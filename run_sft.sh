9e880e8498bccd100e34a15bb2c4a19c2ff7d376
Jingbo Liu <jingbo25@fb.com>
2025-10-25 09:00:51 -0700

    sft13

--- run_sft.sh
+++ run_sft.sh
@@ -1,6 +1,6 @@
 
 for block_id in {0..12}; do
-  CUDA_VISIBLE_DEVICES=7 python trainerSFT.py --prefix_tag cudacoder_eval_4_turn.deepseek_r1.sft06 --epoch_id 0 \
-        --block_id $(printf "%02d" $block_id) --input_tag cudacoder_eval_4_turn.qwen32b_000_$(printf "%02d" $block_id)/deepseek-reasoner-sft \
+  CUDA_VISIBLE_DEVICES=7 python trainerSFT.py --prefix_tag cudacoder_eval_4_turn.deepseek_r1.sft13 --epoch_id 0 \
+        --block_id $(printf "%02d" $block_id) --input_tag cudacoder_eval_4_turn.qwen32b_000_$(printf "%02d" $block_id)/deepseek-reasoner-sft5 \
         --input_dir shared/re_kbeval/.inference/codeGenEval/
-done
+done[]
