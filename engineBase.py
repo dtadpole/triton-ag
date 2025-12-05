79c1d540818313843b3cb8ece973779fb9ea7caa
Jingbo Liu <jingbo25@fb.com>
2025-12-05 14:22:01 -0800

    kevin prompt

--- engineBase.py
+++ engineBase.py
@@ -366,7 +366,7 @@ class EngineBase(ABC):
             dist.is_initialized() and dist.get_rank() == 0
         ) or not dist.is_initialized():
             # Initialize logging
-            random.seed(43)
+            random.seed(46)
             self.config.logging.wandb_run_id = self.prefix_tag + f"{random.randint(0, 99999):05d}"
             self.config.logging.wandb_run_name = (
                 self.prefix_tag + "_" + datetime.now().strftime("%m%d")
