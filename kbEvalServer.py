7315387dcf97a3c873597b6525fd503bd3120b70
Jingbo Liu <jingbo25@fb.com>
2025-12-29 12:48:21 -0800

    kbserver

--- kbEvalServer.py
+++ kbEvalServer.py
@@ -846,7 +846,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--local_host", action="store_true")
     parser.add_argument("--port", type=int, default=8456)
-    parser.add_argument("--workers", type=int, default=100) #
+    parser.add_argument("--workers", type=int, default=40) #
     parser.add_argument("--device", type=str, default="4")
     parser.add_argument("--max_timeout_seconds", type=int, default=240)
     parser.add_argument("--max_critical_time", type=int, default=120, help="Maximum time in seconds for critical section (inside the lock)")
